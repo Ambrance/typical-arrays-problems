983b99695b170eb82328b286fe560198ab49fccb
Ambrance <ambrance@mail.ru>
2020-02-20 16:08:06 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) { return 0; }
+    const arr = array;
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+    return arr[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) { return 0; }
+    const arr = array;
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+    return arr[arr.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) { return 0; }
+    const sum = array.reduce((i, acc) => i + acc);
+    return sum / array.length;
 }
